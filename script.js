ef8b8b4906a70d9adddeecd320ac290adde9c83c
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-14 01:03:42 +0200

    Add round starter and returns results

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+const handChoices = ['rock', 'scissors', 'paper'];
+
 const getComputerChoice = function getComputerChoice() {
-    const handChoices = ['rock', 'scissors', 'paper'];
     let randomNumber = Math.floor(Math.random () * 3);
     
     return handChoices[randomNumber];
+}
+
+const startRound = function startRoundAndReturnWinner(playerChoice, ComputerChoice) {
+
+    const filteredPlayerChoice = playerChoice.toLowerCase();
+    const drawMessage = `It's a draw! Both players used ${filteredPlayerChoice}!`;
+    const playerWinningMessage = `You win! Player ${filteredPlayerChoice} beats ${ComputerChoice}!`;
+    const playerLosingMessage = `You lose! Computer ${ComputerChoice} beats ${filteredPlayerChoice}!`;
+
+    if (!handChoices.includes(filteredPlayerChoice)) {
+        return `${filteredPlayerChoice} is not a hand`;
+    }
+
+    // Checks if there's a draw first.
+    // Checks to see if player beats computer.
+    // Otherwise returns computer as the winner.
+    if (filteredPlayerChoice === ComputerChoice) {
+        return drawMessage;
+    } else if (filteredPlayerChoice === 'rock' && ComputerChoice === 'scissors') {
+        return playerWinningMessage;
+    } else if (filteredPlayerChoice === 'scissors' && ComputerChoice === 'paper') {
+        return playerWinningMessage;
+    } else if (filteredPlayerChoice === 'paper' && ComputerChoice === 'rock') {
+        return playerWinningMessage;
+    } else {
+        return playerLosingMessage;
+    }
 }
