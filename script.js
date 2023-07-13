f753650645264d66a94b8c899c987facb8c7416a
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-14 00:41:13 +0200

    Add computer hand choice getter

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('works');
+const getComputerChoice = function getComputerChoice() {
+    const handChoices = ['rock', 'scissors', 'paper'];
+    let randomNumber = Math.floor(Math.random () * 3);
+    
+    return handChoices[randomNumber];
+}
