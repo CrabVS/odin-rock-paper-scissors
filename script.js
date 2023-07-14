2a7ed9c0432b6128ea57408530ece612a9854aa9
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-14 20:36:08 +0200

    Refactor: Update and refactor all funcs

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const handChoices = ['rock', 'scissors', 'paper'];
 
 let winner = null;
-let round = 0;
-let score = 0;
 
 const getComputerChoice = function getComputerChoice() {
     let randomNumber = Math.floor(Math.random () * 3);
@@ -10,17 +8,15 @@ const getComputerChoice = function getComputerChoice() {
     return handChoices[randomNumber];
 }
 
-const checkGameWinner = function checkForGameWinner(playerScore, computerScore) {
-    if (playerScore === 3) {
-        winner = 'Player';
-    } else if (computerScore === 3) {
-        winner = 'Computer';
-    }
+const checkGameWinner = function checkForGameWinner(score) {
+    if (score.includes(3)) {
+        return score[0] === 3 ? 'Player' : 'Computer';
+    } else return null;
 }
 
 // Calculates the winner and returns the results
-const startRound = function startRoundAndReturnWinner(playerChoice, computerChoice) {
-    
+const checkRoundWinner = function checkForRoundWinner(playerChoice, computerChoice) {
+
     // Checks if there's a draw first. Then if player beats computer. Otherwise returns computer as winner.
     if (playerChoice === computerChoice) {
         return 0;
@@ -35,8 +31,51 @@ const startRound = function startRoundAndReturnWinner(playerChoice, computerChoi
     }
 }
 
+const startRound = function startRound(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const results = checkRoundWinner(playerChoice, computerChoice);
+
+    if (results === 0) console.log('Draw!');
+    else if (results === 1) console.log('Player Wins!');
+    else console.log('Computer Wins!');
+
+    return results;
+}
+
+const addScore = function addScore(playerScore, computerScore, roundResults) {
+    if (roundResults > 0) {
+        playerScore += 1;
+    } else if (roundResults < 0) {
+        computerScore += 1;
+    }
+
+    return [playerScore, computerScore];
+}
+
+const announceWinner = function announceWinner(winner) {
+    console.log(`${winner} is the winner!`)
+}
+
 const game = function gameController() {
-    const buttons
+    let winner = null;
+    // score = [playerScore, computerScore]
+    let score = [0, 0];
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const results = startRound(handChoices[index]);
+            score = addScore(score[0], score[1], results);
+
+            console.log(`Score: ${score[0]} - ${score[1]}`);
+
+            winner = checkGameWinner(score.slice());
+
+            if (winner !== null) {
+                announceWinner(winner);
+            }
+        });
+    });
 }
 
 game();
